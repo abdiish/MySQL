17cf25126f52b57815f19b7515559a5c583f700d
abdiish <ing.ti.upmh@gmail.com>
2020-05-22 01:51:14 -0500

    Uso de CREATE

---
+++ create.sql
@@ -0,0 +1,29 @@
+CREATE TABLE books (
+    book_id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    publisher_id INTEGER UNSIGNED NOT NULL DEFAULT 0,
+    author VARCHAR(100) NOT NULL,
+    title VARCHAR(60) NOT NULL,
+    description TEXT,
+    price DECIMAL(5,2),
+    copies INT NOT NULL DEFAULT 0
+);
+
+CREATE TABLE publishers (
+    publisher_id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    country VARCHAR(20)
+);
+
+CREATE TABLE users (
+    user_id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE actions (
+    action_id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    book_id INTEGER UNSIGNED NOT NULL,
+    user_id INTEGER UNSIGNED NOT NULL,
+    action_type ENUM('venta','prestamo','devolucion') NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
